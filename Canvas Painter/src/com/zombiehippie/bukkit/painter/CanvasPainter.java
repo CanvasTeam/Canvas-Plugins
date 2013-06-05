489aa4e884bfab57a896d2d45cc84b0b5c34385a
Cole R Lawrence <cole@zombiehippie.com>
2013-06-04 20:09:57 -0500

    Skeleton for Canvas Painter

--- Canvas Painter/src/com/zombiehippie/bukkit/painter/CanvasPainter.java
+++ Canvas Painter/src/com/zombiehippie/bukkit/painter/CanvasPainter.java
@@ -1,20 +1,37 @@
 package com.zombiehippie.bukkit.painter;
 
+import java.util.TreeMap;
+
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.zombiehippie.bukkit.painter.listeners.PlayerListeners;
+import com.zombiehippie.bukkit.painter.brushes.Brush;
 
-public class CanvasPainter extends JavaPlugin {
+public class CanvasPainter extends JavaPlugin implements Listener{
+	// PlayerName>Brush
+	TreeMap<String, Brush> brushes = new TreeMap<String, Brush>();
+	
 	@Override
 	public void onEnable(){
 		PluginManager pm = Bukkit.getPluginManager();
 		
-		pm.registerEvents(new PlayerListeners(), this);
+		// Register the EventHandler here, onPlayerInteractEvent
+		pm.registerEvents(this, this);
 	}
 	@Override
 	public void onDisable() {
+		
+	}
+	
+	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent event){
+		Action action = event.getAction();
+		
 		
 	}
 
